606e5540dfecc33bdb6db9ee349d00fda0851b16
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-02-23 15:43:31 -0600

    Whew... Got some pizza toppings loaded...
    Next, let's try to display them in our component...

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaToppingsService, PizzaToppingDisplay } from '../pizza-toppings.service';
 
 @Component({
   selector: 'app-pizza-toppings',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzaToppingsComponent implements OnInit {
 
-  constructor() { }
+  // Magic DI (dependency injection)...
+  constructor(private pizzaSvc: PizzaToppingsService) { }
 
-  ngOnInit(): void {
+  availablePizzaToppings: PizzaToppingDisplay[] = [];
+
+  // Angular "life cycle" function, that gets called when the component is
+  // loaded and ready to display.
+  ngOnInit(): void {  
+
+    this.availablePizzaToppings = this.pizzaSvc.loadPizzaToppings();
+    console.log(this.availablePizzaToppings);
   }
 
 }
