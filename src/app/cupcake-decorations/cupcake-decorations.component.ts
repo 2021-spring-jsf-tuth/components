754b163bbf4ca5ff7cfdbe6644eb4d0bc34719d6
Lisa Clemens <34919562+LelinStam@users.noreply.github.com>
2021-02-23 16:21:39 -0600

    pushing cupcake service

---
+++ src/app/cupcake-decorations/cupcake-decorations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cupcake-decorations',
+  templateUrl: './cupcake-decorations.component.html',
+  styleUrls: ['./cupcake-decorations.component.css']
+})
+export class CupcakeDecorationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
