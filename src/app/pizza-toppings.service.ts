8704360356a67138278b9acc429c14ece82652b0
cvang00 <cvang00@users.noreply.github.com>
2021-02-23 15:43:50 -0600

    Whew... Got some pizza toppings loaded...
    Next, let's try to display them in our component...

--- src/app/pizza-toppings.service.ts
+++ src/app/pizza-toppings.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
 
+export interface PizzaToppingDisplay {
+  name: string; 
+  price: number; 
+  checked: boolean; 
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PizzaToppingsService {
 
   constructor() { }
+
+  //write a new function
+  loadPizzaToppings(): PizzaToppingDisplay[] {
+    // Faux toppings from a web service 
+    const pizzaToppingsFromWebService = [
+      {
+        name: "Pepperoni",
+        price: 1.5
+      }, 
+      {
+        name: "Olives",
+        price: 2.5
+      }
+    ];
+
+    //return a PizzaToppingDisplay array 
+    return pizzaToppingsFromWebService.map(x => ({
+      //spread 
+      ...x, 
+      checked: false 
+    }));
+  }
 }
