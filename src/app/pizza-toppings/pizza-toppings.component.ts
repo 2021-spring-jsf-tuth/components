be81c9c275fea07d88afce2b1d1b5d6087bc6686
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-02-25 15:46:43 -0600

    All/None buttons working...

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -32,4 +32,12 @@ export class PizzaToppingsComponent implements OnInit {
       )
     ; 
   }
+
+  checkAll() {
+    this.availablePizzaToppings = this.availablePizzaToppings.map(x => ({...x, checked: true }));
+  }
+
+  uncheckAll() {
+    this.availablePizzaToppings = this.availablePizzaToppings.map(x => ({...x, checked: false }));
+  }
 }
