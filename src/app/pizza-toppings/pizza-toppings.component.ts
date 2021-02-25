8451a92a711005ccb1ce0f8e82e66e6580093446
jtschiss <51149341+jtschiss@users.noreply.github.com>
2021-02-25 15:50:19 -0600

    got rid of calculate button
    TS getter properties rule

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -22,10 +22,8 @@ export class PizzaToppingsComponent implements OnInit {
     console.log(this.availablePizzaToppings);
   }
 
-  total = 0;
-
-  calculateTotal() {
-    this.total = this.availablePizzaToppings
+  get total() {
+    return this.availablePizzaToppings
       .filter(x => x.checked)
       .reduce(
         (acc, x) => acc + x.price
