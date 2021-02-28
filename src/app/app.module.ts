481aba413cb489cb357b058e39e232558decb887
ayebravo <67244374+ayebravo@users.noreply.github.com>
2021-02-27 18:59:47 -0600

    Generated reservation-date component
    Command: ng generate component reservation-date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReservationDateComponent } from './reservation-date/reservation-date.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    ReservationDateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/reservation-date/reservation-date.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-date',
+  templateUrl: './reservation-date.component.html',
+  styleUrls: ['./reservation-date.component.css']
+})
+export class ReservationDateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
