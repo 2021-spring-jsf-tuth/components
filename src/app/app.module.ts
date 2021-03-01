fbb753002ff2797b482d1c190534dc6afb1c47a0
kkretschmerhensley <k.kretschmerhensley@gmail.com>
2021-03-01 16:06:19 -0600

    ng generate component kkh-snackbar
    Generated a new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { KkhSnackbarComponent } from './kkh-snackbar/kkh-snackbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    KkhSnackbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/kkh-snackbar/kkh-snackbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kkh-snackbar',
+  templateUrl: './kkh-snackbar.component.html',
+  styleUrls: ['./kkh-snackbar.component.css']
+})
+export class KkhSnackbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
