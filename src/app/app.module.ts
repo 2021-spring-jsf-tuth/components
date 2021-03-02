0deaf4cbb6f9377d087df4451a05fba347998b02
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-03-01 18:10:57 -0600

    A D6 roller : - )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { TjsRollD6Component } from './tjs-roll-d6/tjs-roll-d6.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    TjsRollD6Component
   ],
   imports: [
     BrowserModule,
