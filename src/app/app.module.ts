8a8cd595e8aa43998ad0728949580891c81436b4
cvang00 <cvang00@users.noreply.github.com>
2021-03-04 14:04:03 -0600

    Add and Reset Input Chips...
    cv-chips.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
+import { CvChipsComponent } from './cv-chips/cv-chips.component'; 
 
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatCard, MatCardModule } from '@angular/material/card'; 
+import { MatCardModule } from '@angular/material/card'; 
 import { MatButtonModule } from '@angular/material/button'; 
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { CvChipsComponent } from './cv-chips/cv-chips.component'; 
+import { MatChipsModule } from '@angular/material/chips'; 
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,10 @@ import { CvChipsComponent } from './cv-chips/cv-chips.component';
     BrowserAnimationsModule,
     MatCardModule, 
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatChipsModule,
+    MatInputModule,
+    MatIconModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
