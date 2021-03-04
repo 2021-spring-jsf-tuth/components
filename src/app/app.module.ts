a789b4a2682c1c000c8b21a9ff4e7db92ac43d60
cvang00 <cvang00@users.noreply.github.com>
2021-03-04 14:24:41 -0600

    Used custom HTML tag...
    Had error, so created a 'schemas' property and added CUSTOM_ELEMENTS_SCHEMA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -21,6 +21,11 @@ import { MatIconModule } from '@angular/material/icon';
     PizzaToppingsComponent,
     CvChipsComponent
   ],
+  
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ], 
+
   imports: [
     BrowserModule,
     FormsModule,
