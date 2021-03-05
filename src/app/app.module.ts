ad07d26f92d5c4fbf5488dbc906bbe39bc6b5b75
cvang00 <cvang00@users.noreply.github.com>
2021-03-05 14:04:20 -0600

    Revert "Merge branch 'master' into cvang/week-06-thu-in-class"
    This reverts commit 286aed62c0a1438b42b9fe7897b7ebffe2ead106, reversing
    changes made to a789b4a2682c1c000c8b21a9ff4e7db92ac43d60.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 import { CvChipsComponent } from './cv-chips/cv-chips.component'; 
-import { ReservationDateComponent } from './reservation-date/reservation-date.component';
-import { JhProgressBarComponent } from './jh-progress-bar/jh-progress-bar.component';
-import { BearPickerComponent } from './bear-picker/bear-picker.component';
-
-
 
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,48 +12,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button'; 
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips'; 
-import { MatIconModule } from '@angular/material/icon';
-import { TjsD20RollerComponent } from './tjs-d20-roller/tjs-d20-roller.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatSlideToggleModule} from '@angular/material/slide-toggle'
-
-import { KkhSnackbarComponent } from './kkh-snackbar/kkh-snackbar.component';
 import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { TjsRollD6Component } from './tjs-roll-d6/tjs-roll-d6.component';
-import { TjsSquaredComponent } from './tjs-squared/tjs-squared.component';
-import { MdmSlideToggleComponent } from './mdm-slide-toggle/mdm-slide-toggle.component';
-import { JtsRiggedD20Component } from './jts-rigged-d20/jts-rigged-d20.component';
-
-import { MatSliderModule } from '@angular/material/slider';
-import { LalAdditionComponent } from './lal-addition/lal-addition.component';
-
-import { MatRadioModule } from '@angular/material/radio';
-import { MatRippleModule } from '@angular/material/core';
-
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     PizzaToppingsComponent,
-    CvChipsComponent,
-    TjsSquaredComponent,
-    TjsD20RollerComponent,
-    KkhSnackbarComponent,
-    TjsSquaredComponent,
-    ReservationDateComponent,
-    TjsRollD6Component,
-    TjsSquaredComponent,
-    JhProgressBarComponent,
-    MdmSlideToggleComponent,
-    JhProgressBarComponent,
-    JtsRiggedD20Component,
-    BearPickerComponent,
-    LalAdditionComponent
-
-
+    CvChipsComponent
   ],
   
   schemas: [
@@ -74,20 +35,7 @@ import { MatRippleModule } from '@angular/material/core';
     MatCheckboxModule,
     MatChipsModule,
     MatInputModule,
-    MatIconModule,
-    MatCardModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatSliderModule,
-    MatInputModule,
-    MatRadioModule,
-    MatRippleModule,
-    MatFormFieldModule,
-    MatSnackBarModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSliderModule,
-    MatSlideToggleModule
+    MatIconModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
